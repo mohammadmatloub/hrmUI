68aa92e28354f9f87ab69d6c77ad4c95294473d4
mohammad.m <mohammadmatloub20001379@gmail.com>
2025-10-14 10:12:02 +0330

    feat: add-aggregation-row-to-personnel-attendance-list-detail

--- src/app/presentation/components/personnel-attendance/personnel-attendance-list/personnel-attendance-list.ts
+++ src/app/presentation/components/personnel-attendance/personnel-attendance-list/personnel-attendance-list.ts
@@ -6,7 +6,10 @@ import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { DialogModule } from 'primeng/dialog';
 
-import { PersonnelAttendanceMaster,PersonnelAttendanceDetail } from '../../../../core/domain/personnelAttendance.model';
+import {
+  PersonnelAttendanceMaster,
+  PersonnelAttendanceDetail,
+} from '../../../../core/domain/personnelAttendance.model';
 import { PersonnelAttendanceService } from '../../../../infrastructure/services/personnel-attendance.service';
 import { PersonnelAttendanceForm } from '../personnel-attendance-form/personnel-attendance-form';
 import { PersonnelAttendanceImporter } from '../personnel-attendance-importer/personnel-attendance-importer';
@@ -18,7 +21,6 @@ import { YearService } from '../../../../infrastructure/services/year.service';
 import { MonthService } from '../../../../infrastructure/services/month.service';
 import { OrganizationService } from '../../../../infrastructure/services/organization.service';
 import { OccupationService } from '../../../../infrastructure/services/occupation.service';
-import {Department} from '../../../../core/domain/department.model';
 
 @Component({
   selector: 'app-personnel-attendance-list',
@@ -36,10 +38,14 @@ import {Department} from '../../../../core/domain/department.model';
   styleUrl: './personnel-attendance-list.scss',
 })
 export class PersonnelAttendanceList implements OnInit {
+  //#region properties
+
   // Define properties and methods for the component here
   // For example, you might want to fetch attendance data from a service
   personnelAttendances: PersonnelAttendanceMaster[] = [];
   detailsList: PersonnelAttendanceDetail[] = [];
+  // Footer totals (single row)
+  totalsRow: any = {};
   selectedAttendance?: PersonnelAttendanceMaster;
   createDialog: boolean = false;
   detailDialog: boolean = false;
@@ -50,6 +56,10 @@ export class PersonnelAttendanceList implements OnInit {
   organizationList: Organization[] = [];
   occupationList: Occupation[] = [];
 
+  //#endregion
+
+  //#region constructor
+
   constructor(
     private personnelAttendanceService: PersonnelAttendanceService,
     private yearService: YearService,
@@ -58,54 +68,30 @@ export class PersonnelAttendanceList implements OnInit {
     private occupationService: OccupationService
   ) {}
 
+  //#endregion
+
+  //#region angular lifecycle hooks
+
   ngOnInit(): void {
-    // Initialization logic, such as fetching data
     this.loadPersonnelAttendances();
   }
-  loadPersonnelAttendances(): void {
-    this.personnelAttendanceService
-      .getAll()
-      .subscribe((attendances: PersonnelAttendanceMaster[]): void => {
-        this.personnelAttendances = attendances;
-      });
-    this.organizationService
-      .getAll()
-      .subscribe((organizations: Organization[]): void => {
-        this.organizationList = organizations;
-      });
 
-    this.yearService
-      .getAll()
-      .subscribe((years: Year[]): void => {
-      this.yearList = years;
-    });
+  //#endregion
 
-    this.monthService
-      .getAll()
-      .subscribe((months: Month[]): void => {
-      this.monthList = months;
-    });
+  //#region protected methods
 
-    this.occupationService
-      .getAll()
-      .subscribe((occupations: Occupation[]): void => {
-        this.occupationList = occupations;
-      });
-    this.occupationList.length;
-  }
-  // Add methods to handle attendance data, such as fetching, displaying, and managing attendance records
-
-  addPersonnelAttendance(): void {
+  protected addPersonnelAttendance(): void {
     this.selectedAttendance = undefined;
     this.isEditing = false;
     this.createDialog = true;
   }
-  importPersonnelAttendance(): void {
+
+  protected importPersonnelAttendance(): void {
     this.createDialog = false;
     this.importyDialog = true;
   }
 
-  createAttendance(attendance: PersonnelAttendanceMaster): void {
+  protected createAttendance(attendance: PersonnelAttendanceMaster): void {
     // Create new attendance
     console.log('HI Create');
     this.personnelAttendanceService.create(attendance).subscribe((): void => {
@@ -114,29 +100,125 @@ export class PersonnelAttendanceList implements OnInit {
     });
   }
 
-  onCancel(): void {
+  protected onCancel(): void {
     this.createDialog = false;
     this.importyDialog = false;
   }
 
-  detailPersonnelAttendance(personnelAttendance: PersonnelAttendanceMaster): void {
+  protected detailPersonnelAttendance(
+    personnelAttendance: PersonnelAttendanceMaster
+  ): void {
     this.selectedAttendance = { ...personnelAttendance };
 
-    // @ts-ignore
-    for (let attendance of this.selectedAttendance?.personnelAttendanceDetails) {
-      this.detailsList.push(attendance);
+    // reset detailsList and totals
+    this.detailsList = [];
+    this.totalsRow = {};
+
+    // Copy details from selectedAttendance (guard in case of undefined)
+    // @ts-ignore -- model may have personnelAttendanceDetails
+    if (
+      this.selectedAttendance &&
+      this.selectedAttendance.personnelAttendanceDetails
+    ) {
+      for (const attendance of this.selectedAttendance
+        .personnelAttendanceDetails) {
+        this.detailsList.push(attendance);
+      }
     }
 
+    // compute totals for footer rows
+    this.computeTotals();
+
     this.detailDialog = true;
-  //  this.personnelAttendanceService.getById(personnelAttendance.id).subscribe();
-  }
-  editPersonnelAttendance(personnelAttendance: PersonnelAttendanceMaster): void {
-  //  this.personnelAttendanceService.update(personnelAttendance.id,personnelAttendance).subscribe();
+    //  this.personnelAttendanceService.getById(personnelAttendance.id).subscribe();
   }
 
-  deletePersonnelAttendance(personnelAttendance: PersonnelAttendanceMaster): void {
-    this.personnelAttendanceService.delete(personnelAttendance.id!).subscribe((): void => {
-      this.loadPersonnelAttendances();
+  editPersonnelAttendance(
+    personnelAttendance: PersonnelAttendanceMaster
+  ): void {
+    //  this.personnelAttendanceService.update(personnelAttendance.id,personnelAttendance).subscribe();
+  }
+
+  deletePersonnelAttendance(
+    personnelAttendance: PersonnelAttendanceMaster
+  ): void {
+    this.personnelAttendanceService
+      .delete(personnelAttendance.id!)
+      .subscribe((): void => {
+        this.loadPersonnelAttendances();
+      });
+  }
+
+  //#endregion
+
+  //#region private methods
+
+  private loadPersonnelAttendances(): void {
+    this.personnelAttendanceService
+      .getAll()
+      .subscribe((attendances: PersonnelAttendanceMaster[]): void => {
+        this.personnelAttendances = attendances;
+      });
+
+    this.organizationService
+      .getAll()
+      .subscribe((organizations: Organization[]): void => {
+        this.organizationList = organizations;
+      });
+
+    this.yearService.getAll().subscribe((years: Year[]): void => {
+      this.yearList = years;
     });
+
+    this.monthService.getAll().subscribe((months: Month[]): void => {
+      this.monthList = months;
+    });
+
+    this.occupationService
+      .getAll()
+      .subscribe((occupations: Occupation[]): void => {
+        this.occupationList = occupations;
+      });
+
+    this.occupationList.length;
+  }
+  // Add methods to handle attendance data, such as fetching, displaying, and managing attendance records
+
+  /**
+   * Compute totals for numeric columns in detailsList and populate three footer rows.
+   * totals excluding something, or grouped summaries). Currently we'll produce:
+   * - totalsRow: simple sum for most numeric columns
+   */
+  private computeTotals(): void {
+    const sums: any = {
+      attendanceCount: 0,
+      totalHoursWorked: 0,
+      totalMinutesWorked: 0,
+      totalWorked: 0,
+      overtimeWithMultiplier: 0,
+      // model doesn't include overtimeWithOutMultiplier; keep as 0 for footer
+      overtimeWithOutMultiplier: 0,
+      overtimeDaysWorked: 0,
+      overtimeHoursWorked: 0,
+      overtimeMinutesWorked: 0,
+      overtimeTotalWorked: 0,
+    };
+
+    for (const d of this.detailsList) {
+      // only add when value is numeric; coerce strings to numbers where applicable
+      sums.attendanceCount += Number(d.attendanceCount) || 0;
+      sums.totalHoursWorked += Number(d.totalHoursWorked) || 0;
+      sums.totalMinutesWorked += Number(d.totalMinutesWorked) || 0;
+      sums.totalWorked += Number(d.totalWorked) || 0;
+      sums.overtimeWithMultiplier += Number(d.overtimeWithMultiplier) || 0;
+      // overtimeWithOutMultiplier not present on the detail model; skip per-item sum
+      sums.overtimeDaysWorked += Number(d.overtimeDaysWorked) || 0;
+      sums.overtimeHoursWorked += Number(d.overtimeHoursWorked) || 0;
+      sums.overtimeMinutesWorked += Number(d.overtimeMinutesWorked) || 0;
+      sums.overtimeTotalWorked += Number(d.overtimeTotalWorked) || 0;
+    }
+
+    // Assign the computed sums to the single footer row.
+    this.totalsRow = { ...sums };
   }
 }
